c4574ed839731d8e66fdce8612bc94edcf508667
yuqi88 <snow34545@gmail.com>
2026-05-29 12:57:45 -0700

    add file

---
+++ B_product_and_basket_analysis/product_and_basket_analysis_1.sql
@@ -0,0 +1,33 @@
+Product analysis/Market Basket Analysis
+- Which product categories sell the most?
+
+1. customer's basket behavior analysis
+    - Average items per order
+    - Peak-hour/weekend/morning basket sizes
+    - Delivery vs in-person basket behavior
+2. Ancors + Add-ons Analysis
+3. Add-on Analysis
+    - Which drink/drink category lead to add-ons
+    - What are the more frequent add-ons
+    - Is the ancor and add-ons different during different period or channels?
+    - What percentage of coffee orders include food?
+
+    - What are the most common product combinations?
+    - Which combos occur more often than expected?
+    -What products should be bundled together?
+4. Strategic Basket Questions (conclusion from the above 3 quesions)
+Should we create combo deals?
+Which products should be placed near checkout?
+Which items deserve upsell prompts?
+Which products increase AOV most?
+Which products are “gateway items”?
+
+
+Strong metrics to include
+Attachment rate
+Basket size
+Items per order
+Food add-on ratio
+Product pair frequency
+Average order value by basket type
+*/
